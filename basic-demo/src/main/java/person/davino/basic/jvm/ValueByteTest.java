f75d16627cfb9ffe6dc269e9d59b43af3b15cdda
fengjy <fjyuan1991@163.com>
2018-08-08 22:24:48 +0800

    byte value tests

--- basic-demo/src/main/java/person/davino/basic/jvm/ValueByteTest.java
+++ basic-demo/src/main/java/person/davino/basic/jvm/ValueByteTest.java
@@ -1,4 +1,13 @@
 package person.davino.basic.jvm;
 
 public class ValueByteTest {
+
+    public static void main(String[] args) {
+        String s = Integer.toBinaryString(Integer.MAX_VALUE & 0x00000000);
+        System.out.println(s);
+        System.out.println(s.length());
+
+        Integer.toBinaryString(Integer.MAX_VALUE);
+        System.out.println((byte)513);
+    }
 }
