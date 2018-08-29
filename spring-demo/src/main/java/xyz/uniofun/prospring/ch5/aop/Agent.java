2d978c4da4fda0734c4b980faf8131fae9a19489
davino <fjyuan1991@163.com>
2018-08-29 19:40:10 +0800

    spring demo: simple aop

---
+++ spring-demo/src/main/java/xyz/uniofun/prospring/ch5/aop/Agent.java
@@ -0,0 +1,7 @@
+package xyz.uniofun.prospring.ch5.aop;
+
+public class Agent {
+    public void speak() {
+        System.out.println("Bond");
+    }
+}
---
+++ spring-demo/src/main/java/xyz/uniofun/prospring/ch5/aop/SimpleAopDemo.java
@@ -0,0 +1,19 @@
+package xyz.uniofun.prospring.ch5.aop;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+public class SimpleAopDemo {
+
+    public static void main(String[] args) {
+        Agent target = new Agent();
+
+        ProxyFactory proxyFactory = new ProxyFactory();
+        proxyFactory.addAdvice(new AgentDecorator());
+        proxyFactory.setTarget(target);
+
+        Agent proxy = (Agent)proxyFactory.getProxy();
+
+        System.out.println("Speak...");
+        proxy.speak();
+    }
+}
