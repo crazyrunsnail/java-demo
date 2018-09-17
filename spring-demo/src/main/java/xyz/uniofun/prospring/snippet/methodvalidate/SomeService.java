4f3d4ddd9655530974d3e45d189954a9e6da187e
fengjy <fjyuan1991@163.com>
2018-09-17 22:22:02 +0800

    spring-demo: method validation

---
+++ spring-demo/src/main/java/xyz/uniofun/prospring/snippet/methodvalidate/SomeService.java
@@ -0,0 +1,55 @@
+package xyz.uniofun.prospring.snippet.methodvalidate;
+
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class SomeService {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+        Service bean = ctx.getBean(Service.class);
+        bean.s(null);
+    }
+
+    @ComponentScan
+    @Configuration
+    public static class Config {
+        @Bean
+        public Validator validatorFactory() {
+            LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+//            localValidatorFactoryBean.setProviderClass(HibernateValidator.class);
+            return localValidatorFactoryBean;
+        }
+
+        @Bean
+        public MethodValidationPostProcessor methodValidationPostProcessor() {
+            MethodValidationPostProcessor methodValidationPostProcessor = new MethodValidationPostProcessor();
+//            methodValidationPostProcessor.setValidator(validatorAdapter);
+            return methodValidationPostProcessor;
+        }
+
+    }
+
+    @Validated
+    @Component
+    public static class Service {
+        public String s(@NotBlank @NotNull String name) {
+            return name;
+        }
+    }
+}
