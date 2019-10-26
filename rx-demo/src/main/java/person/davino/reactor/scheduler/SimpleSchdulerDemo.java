563b8d0edaf638f8d48d225b52245d04ba5b4476
crazyrunsnail <fjyuan1991@163.com>
2019-10-26 10:11:07 +0800

    rx demo: subscribe以及schedulers

--- rx-demo/src/main/java/person/davino/reactor/scheduler/SimpleSchdulerDemo.java
+++ rx-demo/src/main/java/person/davino/reactor/scheduler/SimpleSchdulerDemo.java
@@ -4,6 +4,7 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleSchdulerDemo {
@@ -12,7 +13,11 @@ public class SimpleSchdulerDemo {
         CountDownLatch cdl = new CountDownLatch(1);
         System.out.println("Runing from Thread: " + Thread.currentThread().getName());
         Mono.fromCallable(SimpleSchdulerDemo::getStringAsyn)
-                .subscribeOn(Schedulers.elastic())
+                .subscribeOn(Schedulers.immediate())    // 使用的是当前线程 main
+//                .subscribeOn(Schedulers.single())     // 使用single-1线程
+//                .subscribeOn(Schedulers.elastic())  // 使用elastic-2线程
+//                .subscribeOn(Schedulers.parallel()) // 多线程, 与CPU核数相关
+//                .subscribeOn(Schedulers.fromExecutor(Executors.newSingleThreadExecutor())) // 允许使用相对应的线程池
                 .subscribe(System.out::println, null, cdl::countDown);
         cdl.await(2, TimeUnit.SECONDS);
     }
@@ -20,6 +25,7 @@ public class SimpleSchdulerDemo {
 
     private static String getStringAsyn() {
         try {
+            System.out.println("Current thread name is :" + Thread.currentThread().getName());
             TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException ite) {
             ite.printStackTrace();
