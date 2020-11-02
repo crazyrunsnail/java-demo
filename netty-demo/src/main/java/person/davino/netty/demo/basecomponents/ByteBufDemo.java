930f074b252f48f5bcb6770897f97a28cd521c3f
crazyrunsnail <fjyuan1991@163.com>
2020-11-02 16:34:21 +0800

    netty: ByteBufDemo

---
+++ netty-demo/src/main/java/person/davino/netty/demo/basecomponents/ByteBufDemo.java
@@ -0,0 +1,47 @@
+package person.davino.netty.demo.basecomponents;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ *  堆内
+ *  direct
+ *  composite
+ */
+public class ByteBufDemo {
+
+    public static void main(String[] args) {
+        // 数据是共享的
+        ByteBuf byteBuf = Unpooled.copiedBuffer("Netty!", StandardCharsets.UTF_8);
+        System.out.println(byteBuf.toString(StandardCharsets.UTF_8));
+        System.out.println(byteBuf.isReadable());
+        System.out.println(byteBuf.isDirect());
+        System.out.println(byteBuf.capacity());
+        System.out.println(byteBuf.readerIndex());
+        System.out.println(byteBuf.writerIndex());
+
+
+        byteBuf.setByte(0, (byte) 'n');
+        System.out.println(">------------<");
+        System.out.println(byteBuf.capacity());
+        System.out.println(byteBuf.readerIndex());
+        System.out.println(byteBuf.writerIndex());
+
+        byteBuf.writeBytes("!".getBytes(StandardCharsets.UTF_8));
+        System.out.println(">------------<");
+        System.out.println(byteBuf.capacity());
+        System.out.println(byteBuf.readerIndex());
+        System.out.println(byteBuf.writerIndex());
+        System.out.println(byteBuf.toString(StandardCharsets.UTF_8));
+
+
+        /*ByteBuf slice = byteBuf.slice(0, 3);
+        byteBuf.setByte(0, (byte) 'J');
+        System.out.println(byteBuf.getByte(0) == slice.getByte(0)); // true*/
+
+
+
+    }
+}
